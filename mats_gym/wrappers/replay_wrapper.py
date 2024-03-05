c96d1190c4b17e54e804a768d922c4e67e6ea0fb
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2024-03-05 22:33:23 +0100

    Add replay wrapper and remove replay functionality from base env.

--- mats_gym/wrappers/replay_wrapper.py
+++ mats_gym/wrappers/replay_wrapper.py
@@ -2,28 +2,41 @@ from typing import Any
 
 import carla
 import numpy as np
-from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
 
 from mats_gym import BaseScenarioEnv
+from mats_gym.envs.replays import SimulationHistory
 from mats_gym.envs.scenario_env_wrapper import BaseScenarioEnvWrapper
 
 
-
 class ReplayWrapper(BaseScenarioEnvWrapper):
-    def __init__(self, env: BaseScenarioEnvWrapper | BaseScenarioEnv) -> None:
+    def __init__(self, env: BaseScenarioEnvWrapper | BaseScenarioEnv, replay_dir: str = "/home/carla") -> None:
         super().__init__(env)
         self._action_records = []
         self._current_episode = []
         self._replay_frames = 0
         self._current_step = 0
+        self._replay_dir = replay_dir
 
     @property
     def num_records(self) -> int:
         return len(self._action_records)
 
+    @property
+    def history(self) -> SimulationHistory:
+        """
+        Returns the simulation history.
+        @return: The simulation history of the current episode.
+        """
+        history = self.client.show_recorder_file_info(
+            f"{self._replay_dir}/scenario-env.log", True
+        )
+        return SimulationHistory(history)
+
     def reset(self, seed: int | None = None, options: dict | None = None) -> tuple[dict, dict[Any, dict]]:
         options = options or {}
+        self.client.stop_recorder()
         obs, info = self.env.reset(seed=seed, options=options)
+        self.client.start_recorder(f"{self._replay_dir}/scenario-env.log")
         replay_options = options.get("replay", {})
         self._current_step = 0
         if replay_options.get("reset", False):
@@ -38,7 +51,8 @@ class ReplayWrapper(BaseScenarioEnvWrapper):
         self._replay_actors = replay_options.get("replay_actors", list(self.env.actors.keys()))
         return obs, info
 
-    def step(self, actions: dict[str, np.ndarray]) -> tuple[dict[str, np.ndarray], dict[str, float], dict[str, bool], dict[str, bool], dict[str, dict]]:
+    def step(self, actions: dict[str, np.ndarray]) -> tuple[
+        dict[str, np.ndarray], dict[str, float], dict[str, bool], dict[str, bool], dict[str, dict]]:
         if self._current_step < self._replay_frames:
             controls = self._action_records[self._replay_episode][self._current_step]
             commands = []
@@ -53,5 +67,3 @@ class ReplayWrapper(BaseScenarioEnvWrapper):
         })
         self._current_step += 1
         return step
-
-
